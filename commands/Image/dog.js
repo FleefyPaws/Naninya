84bffbb400b179d55a4ffebb01f67a52aaa3a687
FleefyPaws <65853789+FleefyPaws@users.noreply.github.com>
2020-08-18 21:33:23 +0530

    Update dog.js

--- commands/Image/dog.js
+++ commands/Image/dog.js
@@ -3,25 +3,34 @@ const {
     MessageEmbed
 } = require("discord.js");
 module.exports.run = async (bot, message, args) => {
-    if (!message.guild.me.hasPermission('EMBED_LINKS') && !message.guild.me.hasPermission('ADMINISTRATOR')) {
-        return message.channel.send(`Please give the bot **Embed Links** Permission`)
+    try {
+        if (!message.guild.me.hasPermission('EMBED_LINKS') && !message.guild.me.hasPermission('ADMINISTRATOR')) {
+            return message.channel.send(`Please give the bot **Embed Links** Permission`)
+        }
+        if (!message.guild.me.hasPermission('MANAGE_MESSAGES') && !message.guild.me.hasPermission('ADMINISTRATOR')) {
+            return message.channel.send(`Please give the bot **Manage Messages** Permission`)
+        }
+        message.channel.startTyping();
+        fetch('https://some-random-api.ml/img/dog').then(res => res.json()).then(body => {
+            if (!body) return message.channel.send('Sorry i\'v broke!');
+            const embed = new MessageEmbed()
+                .setColor(`#32cd32`)
+                .setImage(body.link)
+                .setTitle(`Doggo`)
+                .setURL(body.link)
+                .setTimestamp()
+                .setFooter(`${bot.user.username} by FleeffyPawsYT`)
+            message.channel.stopTyping();
+            message.channel.send(embed);
+        })
+    } catch (e) {
+        console.log(e)
+        const errembed = new MessageEmbed()
+            .setTitle("An error occured")
+            .setColor('#FF000')
+            .setDescription(`Error: ${e}. \nPlease report this error to our support server: **https: //discord.gg/s2ezK4X**`)
+        message.channel.send(errembed)
     }
-    if (!message.guild.me.hasPermission('MANAGE_MESSAGES') && !message.guild.me.hasPermission('ADMINISTRATOR')) {
-        return message.channel.send(`Please give the bot **Manage Messages** Permission`)
-    }
-    message.channel.startTyping();
-    fetch('https://some-random-api.ml/img/dog').then(res => res.json()).then(body => {
-        if (!body) return message.channel.send('Sorry i\'v broke!');
-        const embed = new MessageEmbed()
-            .setColor(`#32cd32`)
-            .setImage(body.link)
-            .setTitle(`Doggo`)
-            .setURL(body.link)
-            .setTimestamp()
-            .setFooter(`${bot.user.username} by FleeffyPawsYT`)
-        message.channel.stopTyping();
-        message.channel.send(embed);
-    })
 }
 
 module.exports.config = {
@@ -31,5 +40,5 @@ module.exports.config = {
     category: "Image",
     timeout: '5000',
     timeoutname: '5 seconds',
-    aliases: ['dogs', 'doggo', 'pup', 'puppy']
+    aliases: ['dogs', 'doggo', 'pup', 'puppy', 'doge']
 }
