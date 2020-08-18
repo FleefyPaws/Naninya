1b2f7cd7ab2f1b18184454170f1d1e4fb71b87e2
FleefyPaws <65853789+FleefyPaws@users.noreply.github.com>
2020-08-18 10:06:57 +0530

    Update ping.js

--- commands/Miscellaneous/ping.js
+++ commands/Miscellaneous/ping.js
@@ -30,7 +30,7 @@ module.exports.run = async (bot, message, args) => {
             .addField('Latencies', [`**❯ Bot Latency: \`${latency}ms\`**`, `**❯ API Latency: \`${Math.round(bot.ws.ping)}ms\`**`])
             .setColor(color)
             .setTimestamp()
-            .setFooter(`${bot.user.isername} by FleeffyPawsYT`);
+            .setFooter(`${bot.user.username} by FleeffyPawsYT`);
 
         message.channel.send(pingdeEmbed);
     } catch (e) {
