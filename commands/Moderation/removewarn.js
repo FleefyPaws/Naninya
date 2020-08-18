bb9c293e7a1c6a7bd6a17a3f016910c95857796a
FleefyPaws <65853789+FleefyPaws@users.noreply.github.com>
2020-08-18 21:16:07 +0530

    Update removewarn.js

--- commands/Moderation/removewarn.js
+++ commands/Moderation/removewarn.js
@@ -80,13 +80,14 @@ module.exports.run = async (bot, message, args) => {
           data.save();
         }
       });
-  } catch (e) {
-    console.log(e)
-    const errembed = new MessageEmbed()
-      .setTitle("An error occured")
-      .setDescription(`Error: ${error}. \nPlease report this error to our support server: **https: //discord.gg/s2ezK4X**`)
-    message.channel.send(errembed)
-  }
+    } catch (e) {
+        console.log(e)
+        const errembed = new MessageEmbed()
+            .setTitle("An error occured")
+            .setColor('#FF0000')
+            .setDescription(`Error: ${e}. \nPlease report this error to our support server: **https: //discord.gg/s2ezK4X**`)
+        message.channel.send(errembed)
+    }
 }
 
 module.exports.config = {
