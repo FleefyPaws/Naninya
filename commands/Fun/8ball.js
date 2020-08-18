2567e13bb010ebac81bccf662110db44e68e2d3c
FleefyPaws <65853789+FleefyPaws@users.noreply.github.com>
2020-08-18 21:38:44 +0530

    Update 8ball.js

--- commands/Fun/8ball.js
+++ commands/Fun/8ball.js
@@ -3,39 +3,39 @@ const {
 } = require("discord.js");
 module.exports.run = async (bot, message, args) => {
     try {
-    if (!message.guild.me.hasPermission('EMBED_LINKS') && !message.guild.me.hasPermission('ADMINISTRATOR')) {
-        return message.channel.send(`Please give the bot **Embed Links** Permission`)
-    }
-    if (!message.guild.me.hasPermission('MANAGE_MESSAGES') && !message.guild.me.hasPermission('ADMINISTRATOR')) {
-        return message.channel.send(`Please give the bot **Manage Messages** Permission`)
-    }
-    const question = args.slice(0).join(' ');
-    if (!question) {
-        message.delete();
-        const nopermembed = new MessageEmbed()
-            .setTitle("❌ You did not specify your question!")
-            .setColor('#FF0000')
-        return message.channel.send(nopermembed).then(msg => msg.delete({
-            timeout: 5000
-        }));
-    }
-    const responses = [
-        'Yes',
-        'No',
-        'Definetly',
-        'Absoloutely',
-        'Not in a million years'
-    ];
-    const response =
-        responses[Math.floor(Math.random() * responses.length)];
-    const Embed = new MessageEmbed()
-        .setTitle(`🎱 8Ball 🎱`)
-        .addField(`Your question:`, `\`\`\`${question}\`\`\``)
-        .addField(`My reply:`, `\`\`\`${response}\`\`\``)
-        .setColor(`#32cd32`)
-        .setFooter(`${bot.user.username} by FleeffyPawsYT`);
-    return message.channel.send(Embed);
-            } catch (e) {
+        if (!message.guild.me.hasPermission('EMBED_LINKS') && !message.guild.me.hasPermission('ADMINISTRATOR')) {
+            return message.channel.send(`Please give the bot **Embed Links** Permission`)
+        }
+        if (!message.guild.me.hasPermission('MANAGE_MESSAGES') && !message.guild.me.hasPermission('ADMINISTRATOR')) {
+            return message.channel.send(`Please give the bot **Manage Messages** Permission`)
+        }
+        const question = args.slice(0).join(' ');
+        if (!question) {
+            message.delete();
+            const nopermembed = new MessageEmbed()
+                .setTitle("❌ You did not specify your question!")
+                .setColor('#FF0000')
+            return message.channel.send(nopermembed).then(msg => msg.delete({
+                timeout: 5000
+            }));
+        }
+        const responses = [
+            'Yes',
+            'No',
+            'Definetly',
+            'Absoloutely',
+            'Not in a million years'
+        ];
+        const response =
+            responses[Math.floor(Math.random() * responses.length)];
+        const Embed = new MessageEmbed()
+            .setTitle(`🎱 8Ball 🎱`)
+            .addField(`Your question:`, `\`\`\`${question}\`\`\``)
+            .addField(`My reply:`, `\`\`\`${response}\`\`\``)
+            .setColor(`#32cd32`)
+            .setFooter(`${bot.user.username} by FleeffyPawsYT`);
+        return message.channel.send(Embed);
+    } catch (e) {
         console.log(e)
         const errembed = new MessageEmbed()
             .setTitle("An error occured")
