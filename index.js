f0986ccd762654ddb5cc8d2b808f69dd16824c3d
FleefyPaws <65853789+FleefyPaws@users.noreply.github.com>
2020-08-21 22:36:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,7 @@ const {
     Client,
     Collection,
 } = require("discord.js");
-const config = require("./config.json");
-const bot = new Client({
-    partials: ["MESSAGE", "CHANNEL", "REACTION"]
-});
+const bot = new Client();
 bot.prefix = process.env.PREFIX;
 ["aliases", "commands"].forEach(x => bot[x] = new Collection());
 ['event', 'command'].forEach(x => require(`./handlers/${x}`)(bot));
