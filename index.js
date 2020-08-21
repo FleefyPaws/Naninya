bd59c35cbbe83d250a403bef8f68886b9cff32ad
FleefyPaws <65853789+FleefyPaws@users.noreply.github.com>
2020-08-21 22:33:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ const config = require("./config.json");
 const bot = new Client({
     partials: ["MESSAGE", "CHANNEL", "REACTION"]
 });
-bot.prefix = config.prefix;
-bot.config = config;
+bot.prefix = process.env.PREFIX;
 ["aliases", "commands"].forEach(x => bot[x] = new Collection());
 ['event', 'command'].forEach(x => require(`./handlers/${x}`)(bot));
 bot.login(process.env.TOKEN);
