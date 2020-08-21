d77546106c3944755c29aeba39c1fd442fc79647
FleefyPaws <65853789+FleefyPaws@users.noreply.github.com>
2020-08-21 22:20:48 +0530

    Update server.js

--- handlers/server.js
+++ handlers/server.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 
 
-app.get("/", function(request, response) {
-    response.sendFile(__dirname + "/webpage/index.html");
+app.use(express.static(__dirname + "webpage"));
+
+// https://expressjs.com/en/starter/basic-routing.html
+app.get("/", (request, response) => {
+  response.sendFile(__dirname + "/webpage/index.html");
 });
-module.exports = router
+
+module.exports = app
