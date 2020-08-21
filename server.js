2d9f63354885406172313c0e36f60bebfa8182ce
FleefyPaws <65853789+FleefyPaws@users.noreply.github.com>
2020-08-21 22:08:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express.Router();
 
 
-router.get("/", function(request, response) {
-  response.sendFile(__dirname + "/webpage/index.html");
+app.get("/", function(request, response) {
+    response.sendFile(__dirname + "/webpage/index.html");
 });
+module.exports = app
