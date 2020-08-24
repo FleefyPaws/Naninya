891389e31981fe86dd9cd2cbddab434411c42257
FleefyPaws <65853789+FleefyPaws@users.noreply.github.com>
2020-08-24 18:21:37 +0530

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,19 +1,14 @@
-const mongoEconomy = require('discord-mongodb-economy')
 module.exports = async (bot) => {
     console.log([
         `Loaded ${bot.commands.size} Commands`,
         'The Bot Is Ready And Running',
         `The Bot is running in ${bot.guilds.cache.size} Servers. Working For ${bot.users.cache.size} Users`
     ].join('\n'));
-    mongoEconomy.connectDatabase(process.env.MONGOURI)
     const statuses = [
         `${bot.guilds.cache.size} servers!`,
         `${bot.users.cache.size} users!`,
         `Now has ${bot.commands.size} Commands`
     ];
-    const mongoCurrency = require('discord-mongo-currency');
-
-    mongoCurrency.connect(process.env.MONGOURI);
     setInterval(() => {
         const status = statuses[Math.floor(Math.random() * statuses.length)];
         bot.user.setActivity(status, {
