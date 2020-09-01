f4967a15bd6798c63dabebaeabcb92597a9467da
FleefyPaws <65853789+FleefyPaws@users.noreply.github.com>
2020-09-01 18:36:36 +0530

    Update unmute.js

--- commands/Moderation/unmute.js
+++ commands/Moderation/unmute.js
@@ -1,7 +1,7 @@
 const {
 	MessageEmbed
 } = require('discord.js');
-const Mute = require('../../models/muterole');
+const Mute = require('../../models/Mute');
 const {
 	isNull
 } = require('lodash');
