c05f51b76f59caf90f7e94666459a85c58d6a3b6
FleefyPaws <65853789+FleefyPaws@users.noreply.github.com>
2020-09-18 14:13:32 +0530

    ok

--- commands/Config/removemuterole.js
+++ commands/Config/removemuterole.js
@@ -1,124 +1,125 @@
 const {
-	MessageEmbed
+    MessageEmbed
 } = require('discord.js');
 const Data = require('../../models/Data');
 const Mute = require('../../models/Mute');
 module.exports.run = async (bot, message) => {
-	try {
-		const nopermembed = new MessageEmbed()
-			.setTitle('❌ You do not have the permission to use this command')
-			.setColor('#FF0000');
-		if (!message.member.hasPermission('ADMINISTRATOR')) {
-			return message.channel.send(nopermembed).then(msg => {
-				if (!message.guild.me.hasPermission('MANAGE_MESSAGES') && !message.guild.me.hasPermission('ADMINISTRATOR')) {
-					msg.delete({
-						timeout: 5000
-					})
-				} else {
-					message.delete()
-					msg.delete({
-						timeout: 5000
-					})
-				}
-			});
-		}
-		if (!message.guild.me.hasPermission('EMBED_LINKS') && !message.guild.me.hasPermission('ADMINISTRATOR')) {
-			return message.channel.send(`Please give the bot **Embed Links** Permission`);
-		}
-		Data.findOne({
-			GuildID: message.guild.id,
-		), async (err, data) => {
-			if (err) console.log(err);
-			if (!data) {
-				const newEmbed = new MessageEmbed()
-					.setTitle('❌ The MuteRole Module is disabled for this server please enable it by using ' + `${bot.prefix}enablemuterole`)
-					.setColor('#FF0000');
-					return message.channel.send(newEmbed).then(msg => {
-						if (!message.guild.me.hasPermission('MANAGE_MESSAGES') && !message.guild.me.hasPermission('ADMINISTRATOR')) {
-							msg.delete({
-								timeout: 5000
-							})
-						} else {
-							message.delete()
-							msg.delete({
-								timeout: 5000
-							})
-						}
-					});
-			} else if (data.MuteRole === false) {
-					const newEmbed = new MessageEmbed()
-						.setTitle('❌ The MuteRole Module is disabled for this server please enable it by using ' + `${bot.prefix}enablemuterole`)
-						.setColor('#FF0000');
-						return message.channel.send(newEmbed).then(msg => {
-							if (!message.guild.me.hasPermission('MANAGE_MESSAGES') && !message.guild.me.hasPermission('ADMINISTRATOR')) {
-								msg.delete({
-									timeout: 5000
-								})
-							} else {
-								message.delete()
-								msg.delete({
-									timeout: 5000
-								})
-							}
-						});
-			} else {
-				Mute.findOneAndDelete({
-					GuildID: message.guild.id
-				},
-					async (err, data) => {
-						if (err) console.log(err);
-						if (!data) {
-							const nope1rmembed = new MessageEmbed()
-								.setTitle(`❌ How will you delete a thing that is not even there!\nPlease do \`${bot.prefix}setmuterole <ROLE | ROLEID>\` to set muterole!`)
-								.setColor('#FF0000');
-							return message.channel.send(nope1rmembed).then(msg => {
-								if (!message.guild.me.hasPermission('MANAGE_MESSAGES') && !message.guild.me.hasPermission('ADMINISTRATOR')) {
-									msg.delete({
-										timeout: 5000
-									})
-								} else {
-									message.delete()
-									msg.delete({
-										timeout: 5000
-									})
-								}
-							});
-						} else {
-							const nope1r4membed = new MessageEmbed()
-								.setTitle(`<:yes:744037966942568539> The muterole has been deleted`)
-								.setColor('#32cd32');
-							return message.channel.send(nope1r4membed).then(msg => {
-								if (!message.guild.me.hasPermission('MANAGE_MESSAGES') && !message.guild.me.hasPermission('ADMINISTRATOR')) {
-									msg.delete({
-										timeout: 5000
-									})
-								} else {
-									message.delete()
-									msg.delete({
-										timeout: 5000
-									})
-								}
-							});
-						}
-					});
-			}
-		})}
-	} catch (err) {
-		console.log(err);
-		const errembed = new MessageEmbed()
-			.setTitle('An error occured')
-			.setColor('#FF0000')
-			.setDescription(`Error: ${err}. \nPlease report this error to our support server: **[Link](https://discord.gg/QTdEFhk)**`);
-		const user = bot.users.cache.get('443278070825091072')
-		user.send(errembed)
-		return message.channel.send(errembed);
-	}
+    try {
+        const nopermembed = new MessageEmbed()
+            .setTitle('❌ You do not have the permission to use this command')
+            .setColor('#FF0000');
+        if (!message.member.hasPermission('ADMINISTRATOR')) {
+            return message.channel.send(nopermembed).then(msg => {
+                if (!message.guild.me.hasPermission('MANAGE_MESSAGES') && !message.guild.me.hasPermission('ADMINISTRATOR')) {
+                    msg.delete({
+                        timeout: 5000
+                    })
+                } else {
+                    message.delete()
+                    msg.delete({
+                        timeout: 5000
+                    })
+                }
+            });
+        }
+        if (!message.guild.me.hasPermission('EMBED_LINKS') && !message.guild.me.hasPermission('ADMINISTRATOR')) {
+            return message.channel.send(`Please give the bot **Embed Links** Permission`);
+        }
+
+        Data.findOne({
+            GuildID: message.guild.id,
+        }, async (err, data) => {
+            if (err) console.log(err);
+            if (!data) {
+                const newEmbed = new MessageEmbed()
+                    .setTitle('❌ The MuteRole Module is disabled for this server please enable it by using ' + `${bot.prefix}enablemuterole`)
+                    .setColor('#FF0000');
+                return message.channel.send(newEmbed).then(msg => {
+                    if (!message.guild.me.hasPermission('MANAGE_MESSAGES') && !message.guild.me.hasPermission('ADMINISTRATOR')) {
+                        msg.delete({
+                            timeout: 5000
+                        })
+                    } else {
+                        message.delete()
+                        msg.delete({
+                            timeout: 5000
+                        })
+                    }
+                });
+            } else if (data.MuteRole === false) {
+                const newEmbed = new MessageEmbed()
+                    .setTitle('❌ The MuteRole Module is disabled for this server please enable it by using ' + `${bot.prefix}enablemuterole`)
+                    .setColor('#FF0000');
+                return message.channel.send(newEmbed).then(msg => {
+                    if (!message.guild.me.hasPermission('MANAGE_MESSAGES') && !message.guild.me.hasPermission('ADMINISTRATOR')) {
+                        msg.delete({
+                            timeout: 5000
+                        })
+                    } else {
+                        message.delete()
+                        msg.delete({
+                            timeout: 5000
+                        })
+                    }
+                });
+            } else {
+                Mute.findOneAndDelete({
+                    GuildID: message.guild.id
+                },
+                    async (err, data) => {
+                        if (err) console.log(err);
+                        if (!data) {
+                            const nope1rmembed = new MessageEmbed()
+                                .setTitle(`❌ How will you delete a thing that is not even there!\nPlease do \`${bot.prefix}setmuterole <ROLE | ROLEID>\` to set muterole!`)
+                                .setColor('#FF0000');
+                            return message.channel.send(nope1rmembed).then(msg => {
+                                if (!message.guild.me.hasPermission('MANAGE_MESSAGES') && !message.guild.me.hasPermission('ADMINISTRATOR')) {
+                                    msg.delete({
+                                        timeout: 5000
+                                    })
+                                } else {
+                                    message.delete()
+                                    msg.delete({
+                                        timeout: 5000
+                                    })
+                                }
+                            });
+                        } else {
+                            const nope1r4membed = new MessageEmbed()
+                                .setTitle(`<:yes:744037966942568539> The muterole has been deleted`)
+                                .setColor('#32cd32');
+                            return message.channel.send(nope1r4membed).then(msg => {
+                                if (!message.guild.me.hasPermission('MANAGE_MESSAGES') && !message.guild.me.hasPermission('ADMINISTRATOR')) {
+                                    msg.delete({
+                                        timeout: 5000
+                                    })
+                                } else {
+                                    message.delete();
+                                    msg.delete({
+                                        timeout: 5000
+                                    });
+                                }
+                            });
+                        }
+                    });
+            }
+        })
+    } catch (err) {
+        console.log(err);
+        const errembed = new MessageEmbed()
+            .setTitle('An error occured')
+            .setColor('#FF0000')
+            .setDescription(`Error: ${err}. \nPlease report this error to our support server: **[Link](https://discord.gg/QTdEFhk)**`);
+        const user = bot.users.cache.get('443278070825091072')
+        user.send(errembed)
+        return message.channel.send(errembed);
+    }
 };
 
 module.exports.config = {
-	name: 'removemuterole',
-	description: 'Removes the muterole of a server',
-	category: 'Config',
-	timeout: 5000,
-	accessableby: 'Admins'
+    name: 'removemuterole',
+    description: 'Removes the muterole of a server',
+    category: 'Config',
+    timeout: 5000,
+    accessableby: 'Admins'
 };
--- commands/Config/setmuterole.js
+++ commands/Config/setmuterole.js
@@ -1,109 +1,110 @@
 const {
-	MessageEmbed
+    MessageEmbed
 } = require('discord.js');
 const Data = require('../../models/Data');
 const Mute = require('../../models/Mute');
-module.exports.run = async (bot, message, args) => {
-	try {
-		const nopermembed = new MessageEmbed()
-			.setTitle('❌ You do not have the permission to use this command')
-			.setColor('#FF0000');
-		if (!message.guild.me.hasPermission('EMBED_LINKS') && !message.guild.me.hasPermission('ADMINISTRATOR')) {
-			return message.channel.send(`Please give the bot **Embed Links** Permission`);
-		}
-		if (!message.member.hasPermission('ADMINISTRATOR')) {
-			return message.channel.send(nopermembed).then(msg => {
-				if (!message.guild.me.hasPermission('MANAGE_MESSAGES') && !message.guild.me.hasPermission('ADMINISTRATOR')) {
-					msg.delete({
-						timeout: 5000
-					})
-				} else {
-					message.delete()
-					msg.delete({
-						timeout: 5000
-					})
-				}
-			});
-		}
-		const role = message.mentions.roles.first() ? message.mentions.roles.first() : message.guild.roles.fetch(rolee => rolee.id === args[0]);
-		if (!role) {
-			const no13permembed = new MessageEmbed()
-				.setTitle('❌ Please mention a role or give the role id')
-				.setColor('#FF0000');
-			return message.channel.send(no13permembed).then(msg => {
-				if (!message.guild.me.hasPermission('MANAGE_MESSAGES') && !message.guild.me.hasPermission('ADMINISTRATOR')) {
-					msg.delete({
-						timeout: 5000
-					})
-				} else {
-					message.delete()
-					msg.delete({
-						timeout: 5000
-					})
-				}
-			});
-		}
-		Mute.findOne({
-			RoleID: role.id,
-			GuildID: message.guild.id
-		},
-			async (err, data) => {
-				if (err) console.log(err);
-				if (!data) {
-					const newMuteRole = new Mute({
-						RoleID: role.id,
-						GuildID: message.guild.id
-					});
-					const no1permembed = new MessageEmbed()
-						.setTitle('<:yes:744037966942568539> The muterole has been for the server')
-						.setColor('#32CD32');
-					return newMuteRole.save().then(message.channel.send(no1permembed).then(msg => {
-						if (!message.guild.me.hasPermission('MANAGE_MESSAGES') && !message.guild.me.hasPermission('ADMINISTRATOR')) {
-							msg.delete({
-								timeout: 5000
-							})
-						} else {
-							message.delete()
-							msg.delete({
-								timeout: 5000
-							})
-						}
-					}));
-				} else {
-					const no1permembed = new MessageEmbed()
-						.setTitle(`❌ The mute role is already set for this server!\nPlease do \`${bot.prefix}removemuterole\` to remove muterole!`)
-						.setColor('#FF0000');
-					return message.channel.send(no1permembed).then(msg => {
-						if (!message.guild.me.hasPermission('MANAGE_MESSAGES') && !message.guild.me.hasPermission('ADMINISTRATOR')) {
-							msg.delete({
-								timeout: 5000
-							})
-						} else {
-							message.delete()
-							msg.delete({
-								timeout: 5000
-							})
-						}
-					});
-				}
-			});
-	} catch (err) {
-		console.log(err);
-		const errembed = new MessageEmbed()
-			.setTitle('An error occured')
-			.setColor('#FF0000')
-			.setDescription(`Error: ${err}. \nPlease report this error to our support server: **[Link](https://discord.gg/QTdEFhk)**`);
-		const user = bot.users.cache.get('443278070825091072')
-		user.send(errembed)
-		return message.channel.send(errembed);
-	}
+module.exports.run = async (bot, message) => {
+    try {
+        const nopermembed = new MessageEmbed()
+            .setTitle('❌ You do not have the permission to use this command')
+            .setColor('#FF0000');
+        if (!message.guild.me.hasPermission('EMBED_LINKS') && !message.guild.me.hasPermission('ADMINISTRATOR')) {
+            return message.channel.send(`Please give the bot **Embed Links** Permission`);
+        }
+        if (!message.member.hasPermission('ADMINISTRATOR')) {
+            return message.channel.send(nopermembed).then(msg => {
+                if (!message.guild.me.hasPermission('MANAGE_MESSAGES') && !message.guild.me.hasPermission('ADMINISTRATOR')) {
+                    msg.delete({
+                        timeout: 5000
+                    })
+                } else {
+                    message.delete()
+                    msg.delete({
+                        timeout: 5000
+                    })
+                }
+            });
+        }
+        Data.findOne({
+            GuildID: message.guild.id
+        }, async (err, message) => {
+            if (err) console.log(err);
+            if (!data) {
+                const newData = new Data({
+                    GuildID: message.guild.id,
+                    MuteRole: false,
+                    ModLogs: false
+                })
+                newData.save();
+                const newEmbed = new MessageEmbed()
+                    .setTitle('❌ The MuteRole Module is disabled please enable it by using ' + `${bot.prefix}enablemuterole`)
+                    .setColor('#FF0000')
+                return message.channel.send(newEmbed)
+            } else if (data.MuteRole === false) {
+                const newEmbed = new MessageEmbed()
+                    .setTitle('❌ The MuteRole Module is disabled please enable it by using ' + `${bot.prefix}enablemuterole`)
+                    .setColor('#FF0000')
+                return message.channel.send(newEmbed)
+            } else {
+
+                Mute.findOne({
+                    GuildID: message.guild.id
+                },
+                    async (err, data) => {
+                        if (err) console.log(err);
+                        if (!data) {
+                            const nope1rmembed = new MessageEmbed()
+                                .setTitle(`❌ The mute role has not been set for this server.\nSet it up by using this command \`${bot.prefix}setmuterole <ROLE>\`!`)
+                                .setColor('#FF0000');
+                            return message.channel.send(nope1rmembed).then(msg => {
+                                if (!message.guild.me.hasPermission('MANAGE_MESSAGES') && !message.guild.me.hasPermission('ADMINISTRATOR')) {
+                                    msg.delete({
+                                        timeout: 5000
+                                    })
+                                } else {
+                                    message.delete()
+                                    msg.delete({
+                                        timeout: 5000
+                                    })
+                                }
+                            });
+                        } else {
+                            const role = message.guild.roles.cache.get(data.RoleID);
+                            const nope1r3membed = new MessageEmbed()
+                                .setTitle(`<:yes:744037966942568539> The mute role for \`${message.guild.name}\` is ${role.name}`)
+                                .setColor('#32cd32');
+                            return message.channel.send(nope1r3membed).then(msg => {
+                                if (!message.guild.me.hasPermission('MANAGE_MESSAGES') && !message.guild.me.hasPermission('ADMINISTRATOR')) {
+                                    msg.delete({
+                                        timeout: 5000
+                                    })
+                                } else {
+                                    message.delete()
+                                    msg.delete({
+                                        timeout: 5000
+                                    })
+                                }
+                            });
+                        }
+                    });
+            }
+        })
+    } catch (err) {
+        console.log(err);
+        const errembed = new MessageEmbed()
+            .setTitle('An error occured')
+            .setColor('#FF0000')
+            .setDescription(`Error: ${err}. \nPlease report this error to our support server: **[Link](https://discord.gg/QTdEFhk)**`);
+        const user = bot.users.cache.get('443278070825091072')
+        user.send(errembed)
+        return message.channel.send(errembed);
+    }
 };
 
 module.exports.config = {
-	name: 'setmuterole',
-	description: 'Sets the muterole of a server',
-	usage: '<ROLE>',
-	category: 'Config',
-	timeout: 5000,
-	accessableby: 'Admins'
+    name: 'muterole',
+    description: 'Views the muterole of a server',
+    category: 'Moderation',
+    timeout: 5000,
+    accessableby: 'Moderator',
 };
