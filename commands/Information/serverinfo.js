1c152ea4c2fe1cf3a4795a44d6fa221dde49247a
FleefyPaws <65853789+FleefyPaws@users.noreply.github.com>
2020-09-20 16:06:39 +0530

    testing

--- commands/Information/serverinfo.js
+++ commands/Information/serverinfo.js
@@ -49,39 +49,38 @@ module.exports.run = async (bot, message, args) => {
 				dynamic: true
 			}))
 			.addField('General', [
-				`**Name:** ${message.guild.name}`,
-				`**ID:** ${message.guild.id}`,
-				`**Owner:** ${message.guild.owner.user.tag} (${message.guild.ownerID})`,
-				`**Region:** ${regions[message.guild.region]}`,
-				`**Boost Tier:** ${message.guild.premiumTier ? `Tier ${message.guild.premiumTier}` : 'None'}`,
-				`**Explicit Filter:** ${filterLevels[message.guild.explicitContentFilter]}`,
-				`**Verification Level:** ${verificationLevels[message.guild.verificationLevel]}`,
-				`**Time Created:** ${moment(message.guild.createdTimestamp).format('LT')} ${moment(message.guild.createdTimestamp).format('LL')} ${moment(message.guild.createdTimestamp).fromNow()}`,
+				`**❯ Name:** ${message.guild.name}`,
+				`**❯ ID:** ${message.guild.id}`,
+				`**❯ Owner:** ${message.guild.owner.user.tag} (${message.guild.ownerID})`,
+				`**❯ Region:** ${regions[message.guild.region]}`,
+				`**❯ Boost Tier:** ${message.guild.premiumTier ? `Tier ${message.guild.premiumTier}` : 'None'}`,
+				`**❯ Explicit Filter:** ${filterLevels[message.guild.explicitContentFilter]}`,
+				`**❯ Verification Level:** ${verificationLevels[message.guild.verificationLevel]}`,
+				`**❯ Time Created:** ${moment(message.guild.createdTimestamp).format('LT')} ${moment(message.guild.createdTimestamp).format('LL')} ${moment(message.guild.createdTimestamp).fromNow()}`,
 				'\u200b'
 			])
 			.addField('Statistics', [
-				`**Role Count:** ${roles.length}`,
-				`**Emoji Count:** ${emojis.size}`,
-				`**Regular Emoji Count:** ${emojis.filter(emoji => !emoji.animated).size}`,
-				`**Animated Emoji Count:** ${emojis.filter(emoji => emoji.animated).size}`,
-				`**Member Count:** ${message.guild.memberCount}`,
-				`**Humans:** ${members.filter(member => !member.user.bot).size}`,
-				`**Bots:** ${members.filter(member => member.user.bot).size}`,
-				`**Text Channels:** ${channels.filter(channel => channel.type === 'text').size}`,
-				`**Voice Channels:** ${channels.filter(channel => channel.type === 'voice').size}`,
-				`**Boost Count:** ${message.guild.premiumSubscriptionCount || '0'}`,
+				`**❯ Role Count:** ${roles.length}`,
+				`**❯ Emoji Count:** ${emojis.size}`,
+				`**❯ Regular Emoji Count:** ${emojis.filter(emoji => !emoji.animated).size}`,
+				`**❯ Animated Emoji Count:** ${emojis.filter(emoji => emoji.animated).size}`,
+				`**❯ Member Count:** ${message.guild.memberCount}`,
+				`**❯ Humans:** ${members.filter(member => !member.user.bot).size}`,
+				`**❯ Bots:** ${members.filter(member => member.user.bot).size}`,
+				`**❯ Text Channels:** ${channels.filter(channel => channel.type === 'text').size}`,
+				`**❯ Voice Channels:** ${channels.filter(channel => channel.type === 'voice').size}`,
+				`**❯ Boost Count:** ${message.guild.premiumSubscriptionCount || '0'}`,
 				'\u200b'
 			])
 			.addField('Presence', [
-				`<:online:731504589400178802>: ${members.filter(member => member.presence.status === 'online').size}`,
-				`<:idle:731504868786700348>: ${members.filter(member => member.presence.status === 'idle').size}`,
-				`<:dnd:731504589769277500>: ${members.filter(member => member.presence.status === 'dnd').size}`,
-				`<:offline:731504590255685683>: ${members.filter(member => member.presence.status === 'offline').size}`,
+				`**❯ Online:** ${members.filter(member => member.presence.status === 'online').size}`,
+				`**❯ Idle:** ${members.filter(member => member.presence.status === 'idle').size}`,
+				`**❯ Do Not Disturb:** ${members.filter(member => member.presence.status === 'dnd').size}`,
+				`**❯ Offline:** ${members.filter(member => member.presence.status === 'offline').size}`,
 				'\u200b'
 			])
-		embed.addField(`Roles [${roles.length - 1}]`, roles.length < 10 ? roles.join(', ') : roles.length > 10 ? trimArray(roles) : 'None')
-		embed.setTimestamp()
-		embed.setFooter(`${bot.user.username} by FleeffyPawsYT`);
+			.addField(`Roles [${roles.length - 1}]`, roles.length < 10 ? roles.join(', ') : roles.length > 10 ? this.client.utils.trimArray(roles) : 'None')
+			.setTimestamp();
 		message.channel.send(embed);
 	} catch (err) {
 		console.log(err);
