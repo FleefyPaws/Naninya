c825708177dbcf5b48bea614c5b8836c808e014b
FleefyPaws <65853789+FleefyPaws@users.noreply.github.com>
2020-09-20 16:09:06 +0530

    Update serverinfo.js

--- commands/Information/serverinfo.js
+++ commands/Information/serverinfo.js
@@ -49,27 +49,27 @@ module.exports.run = async (bot, message, args) => {
 				dynamic: true
 			}))
 			.addField('General', [
-				`**❯ Name:** ${message.guild.name}`,
-				`**❯ ID:** ${message.guild.id}`,
-				`**❯ Owner:** ${message.guild.owner.user.tag} (${message.guild.ownerID})`,
-				`**❯ Region:** ${regions[message.guild.region]}`,
-				`**❯ Boost Tier:** ${message.guild.premiumTier ? `Tier ${message.guild.premiumTier}` : 'None'}`,
-				`**❯ Explicit Filter:** ${filterLevels[message.guild.explicitContentFilter]}`,
-				`**❯ Verification Level:** ${verificationLevels[message.guild.verificationLevel]}`,
-				`**❯ Time Created:** ${moment(message.guild.createdTimestamp).format('LT')} ${moment(message.guild.createdTimestamp).format('LL')} ${moment(message.guild.createdTimestamp).fromNow()}`,
+				`**Name:** ${message.guild.name}`,
+				`**ID:** ${message.guild.id}`,
+				`**Owner:** ${message.guild.owner.user.tag} (${message.guild.ownerID})`,
+				`**Region:** ${regions[message.guild.region]}`,
+				`**Boost Tier:** ${message.guild.premiumTier ? `Tier ${message.guild.premiumTier}` : 'None'}`,
+				`**Explicit Filter:** ${filterLevels[message.guild.explicitContentFilter]}`,
+				`**Verification Level:** ${verificationLevels[message.guild.verificationLevel]}`,
+				`**Time Created:** ${moment(message.guild.createdTimestamp).format('LT')} ${moment(message.guild.createdTimestamp).format('LL')} ${moment(message.guild.createdTimestamp).fromNow()}`,
 				'\u200b'
 			])
 			.addField('Statistics', [
-				`**❯ Role Count:** ${roles.length}`,
-				`**❯ Emoji Count:** ${emojis.size}`,
-				`**❯ Regular Emoji Count:** ${emojis.filter(emoji => !emoji.animated).size}`,
-				`**❯ Animated Emoji Count:** ${emojis.filter(emoji => emoji.animated).size}`,
-				`**❯ Member Count:** ${message.guild.memberCount}`,
-				`**❯ Humans:** ${members.filter(member => !member.user.bot).size}`,
-				`**❯ Bots:** ${members.filter(member => member.user.bot).size}`,
-				`**❯ Text Channels:** ${channels.filter(channel => channel.type === 'text').size}`,
-				`**❯ Voice Channels:** ${channels.filter(channel => channel.type === 'voice').size}`,
-				`**❯ Boost Count:** ${message.guild.premiumSubscriptionCount || '0'}`,
+				`**Role Count:** ${roles.length}`,
+				`**Emoji Count:** ${emojis.size}`,
+				`**Regular Emoji Count:** ${emojis.filter(emoji => !emoji.animated).size}`,
+				`**Animated Emoji Count:** ${emojis.filter(emoji => emoji.animated).size}`,
+				`**Member Count:** ${message.guild.memberCount}`,
+				`**Humans:** ${members.filter(member => !member.user.bot).size}`,
+				`**Bots:** ${members.filter(member => member.user.bot).size}`,
+				`**Text Channels:** ${channels.filter(channel => channel.type === 'text').size}`,
+				`**Voice Channels:** ${channels.filter(channel => channel.type === 'voice').size}`,
+				`**Boost Count:** ${message.guild.premiumSubscriptionCount || '0'}`,
 				'\u200b'
 			])
 			.addField('Presence', [
